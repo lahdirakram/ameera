938af4c90972a65cd105914ddb3ff87e332ff444
lahdirakram <akramdzbuisness@gmail.com>
2018-10-01 15:34:32 +0000

    new version

--- vendeur/views.py
+++ vendeur/views.py
@@ -73,7 +73,7 @@ def vendre(request):
 			history.objects.filter(hist_date=datetime.date.today(),hist_prod_id_id=0).delete()
 		else:
 			b=a[0]
-			b.hist_nbr_sell =b.hist_nbr_sell + int(qte)
+			b.hist_nbr_sell = b.hist_nbr_sell + int(qte)
 			b.hist_money += int(qte)*price
 			b.save()
 
@@ -142,13 +142,9 @@ def action(request):
 		return redirect('home')
 	return render(request,'main_pages/action.html',{'user':user})
 def home(request):
-<<<<<<< HEAD
-=======
 	a=history.objects.filter(hist_date=datetime.date.today())
 	if(not a.exists()):
 		history.objects.create(hist_date=datetime.date.today(),hist_nbr_sell=0,hist_money=0,hist_prod_id=Product.objects.get(id=0),hist_prix_sell=None,hist_etat=1)
-			
->>>>>>> a22292ab6849ff850182a9985ff43cc51a761b18
 	if request.method == "POST":
 		form=Connect(request.POST)
 		if form.is_valid():
